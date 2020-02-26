fbd5e382a254df3d57cb41a5fab204ca25ecbe33
abiguime <2597434002@qq.com>
2020-02-26 20:18:35 +0000

    26feb2020

--- MyShopService/build/generated-sources/ap-source-output/entites/Client_.java
+++ MyShopService/build/generated-sources/ap-source-output/entites/Client_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2019-09-04T11:51:21")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2019-09-04T12:02:53")
 @StaticMetamodel(Client.class)
 public class Client_ { 
 
